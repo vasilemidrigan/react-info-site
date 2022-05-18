b9128d74f48743ea5d92ee0bbb91152aea6dae00
Vasile Midrigan <111rigan111@gmail.com>
2022-05-18 05:30:13 +0100

    reset project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
 import React from "react";
 
-import Component1Parent from "./components/Component--1--parent";
-import Component2Parent from "./components/Component--2--parent";
-import Component3Parent from "./components/Component--3--parent";
 // Import Styles
 import "./index.css";
 
-export default function App() {
-  return (
-    <div className="app--wrapper">
-      <Component1Parent />
-      <hr />
-      <Component2Parent />
-      <hr />
-      <Component3Parent />
-      <hr />
-    </div>
-  );
-}
+export default function App() {}
--- src/components/Component--1--child.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Component1Child(props) {
-  const bgColor = {
-    backgroundColor: props.on ? "#36AE7C" : "#EB5353",
-  };
-
-  return <div className="component1Child" style={bgColor}></div>;
-}
